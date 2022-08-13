e722254034d71bb7c2de5e36224f07e498baad08
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2022-08-13 18:49:57 +0300

    Сделано: секция хедера, секция баннера, секция 'кто здесь', меню навигации

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,18 @@
+import './Navigation.scss'
+
+function Navigation() {
+
+    return (
+        <div className={'navigation-wrp'}>
+            <nav className={'nav'}>
+                <a className={'nav__item nav__item--active'} href={'id'}>Бинс</a>
+                <a className={'nav__item'} href={'id'}>Кейсы</a>
+                <div className={'nav__spacer'}></div>
+                <a className={'nav__item'} href={'id'}>Войти</a>
+                <a className={'nav__item nav__item--discuss-prj'} href={'id'}>Обсудить проект</a>
+            </nav>
+        </div>
+    )
+}
+
+export default Navigation;
